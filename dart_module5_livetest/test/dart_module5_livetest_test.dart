6d94fa59178fd5783242839983257ff2409cb042
Jahidul haque fahad <jahidulh829@gmail.com>
2025-06-25 23:33:44 +0600

    Done Livetest

---
+++ dart_module5_livetest/test/dart_module5_livetest_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_module5_livetest/dart_module5_livetest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
